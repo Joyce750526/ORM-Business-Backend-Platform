cb6229dce44a3d8a0e65522019ada9e6c2374ccf
Joyce Chen <joyceideas@outlook.com>
2022-10-28 07:41:41 -0400

    removed extra common

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -4,7 +4,7 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
@@ -25,7 +25,7 @@ Product.init(
       allowNull: false,
       validate: {
         isDecimal: true,
-      },
+      }
     },
     stock: {
       type: DataTypes.STRING,
@@ -33,7 +33,7 @@ Product.init(
       defaultValue: 10,
       validate: {
         isNumeric: true,
-      },
+      }
     },
     category_id: {
       type: DataTypes.STRING,
@@ -41,7 +41,7 @@ Product.init(
         model: "category",
         key: "id",
         unique: false,
-      },
+      }
     },
   },
   {
